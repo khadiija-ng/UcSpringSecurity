53c95cac7a99bfee4592a4d15eb3f9f0d7c77489
unknown <niangkhady171@gmail.com>
2024-10-05 10:35:53 +0000

    Spring security Test

---
+++ src/main/java/com/ucad/UcadSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.ucad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcadSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcadSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucad/UcadSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcadSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
